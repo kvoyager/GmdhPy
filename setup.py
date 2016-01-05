140d54e826dc8ff402693f84ef27dbb3dd28726c
kvoyager <konstantin.kolokolov@gmail.com>
2016-01-05 16:44:03 +0300

    bug fix in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='GmdhPy',
       url='https://github.com/kvoyager/GmdhPy',
       download_url='https://github.com/kvoyager/GmdhPy/archive/master.zip',
       license='MIT',
-      install_requires=['numpy', 'six', 'sklearn'],
+      install_requires=['numpy', 'six', 'scikit-learn'],
       extras_require={
           'graphviz': ['graphviz', 'matplotlib'],
       },
