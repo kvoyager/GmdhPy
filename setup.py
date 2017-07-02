29e185253666b4c777be02e07dbf30888eba9d57
kvoyager <konstantin.kolokolov@gmail.com>
2017-07-02 13:34:44 +0300

    remove bat file

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import find_packages
 
 
 setup(name='GmdhPy',
-      version='0.1.1a',
-      description='Multilayered group method of data handling of Machine learning for Python',
+      version='2.0',
+      description='Self-organizing deep learning polynomial neural network for Python (Multilayered group method of data handling)',
       author='kvoyager',
       author_email='konstantin.kolokolov@gmail.com',
       url='https://github.com/kvoyager/GmdhPy',
       download_url='https://github.com/kvoyager/GmdhPy/archive/master.zip',
       license='MIT',
-      install_requires=['numpy', 'six', 'scikit-learn'],
+      install_requires=['numpy', 'six', 'scikit-learn', 'multiprocessing', 'Enum', 'pandas'],
       extras_require={
           'graphviz': ['graphviz', 'matplotlib'],
       },
